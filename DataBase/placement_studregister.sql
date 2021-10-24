866abf0a658dd3c4cf26691828d384ea1f47bb6b
Sonyvanze0037 <90917879+Sonyvanze0037@users.noreply.github.com>
2021-10-24 23:23:50 +0530

    Add files via upload

---
+++ DataBase/placement_studregister.sql
@@ -0,0 +1,59 @@
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: placement
+-- ------------------------------------------------------
+-- Server version	5.6.23-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `studregister`
+--
+
+DROP TABLE IF EXISTS `studregister`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `studregister` (
+  `stud_id` int(11) NOT NULL AUTO_INCREMENT,
+  `stud_name` varchar(255) DEFAULT NULL,
+  `stud_email` varchar(255) DEFAULT NULL,
+  `stud_mob_no` varchar(90) DEFAULT NULL,
+  `stud_degree` varchar(255) DEFAULT NULL,
+  `pass_year` int(12) DEFAULT NULL,
+  `course_name` varchar(255) DEFAULT NULL,
+  `stud_pass` varchar(255) DEFAULT NULL,
+  `stud_profile` longblob NOT NULL,
+  `stud_attends` int(10) DEFAULT NULL,
+  PRIMARY KEY (`stud_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `studregister`
+--
+
+LOCK TABLES `studregister` WRITE;
+/*!40000 ALTER TABLE `studregister` DISABLE KEYS */;
+INSERT INTO `studregister` VALUES (1,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'',NULL),(2,'swami samrath','sony@gmail.com','22222','BE IT',2021,'wb','123456','',NULL),(3,'kirshna','kirshna@gmail.com','8308476298','BE IT',2021,'St','123456','',NULL),(4,'prachi','sony@gmail.com','8308476298','BE IT',2021,'wb','123456','',NULL),(5,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'',NULL),(6,'sunil','sunil@gmail.com','8308476298','BE IT',2021,'St','123456','',NULL),(7,'swami','sony@gmail.com','830871256','BE IT',2021,'Software Testing','123456',_binary 'download.jpg',0),(8,'gayatri','gaytari123@gmail.com','997004152','BE COMP',2021,'Web Development','GAY@123',_binary 'document and upload.PNG',0);
+/*!40000 ALTER TABLE `studregister` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-10-23 10:00:18
